886c48e71a3e0aa52daf1a755447505fcfd7b7d4
Dinosaurrik <78600651+Darya-Tolmeneva@users.noreply.github.com>
2025-11-04 18:06:06 +0300

    fix: operation

--- interpreter/src/models/nodes/OperationNode.java
+++ interpreter/src/models/nodes/OperationNode.java
@@ -7,17 +7,23 @@ import models.token.TokenType;
 import models.token.Token;
 
 public class OperationNode extends AstNode {
-    String operator;
+	String operator;
 	ArrayList<AstNode> operands;
+	TokenType type;
 
 	public OperationNode(Token operator, ArrayList<AstNode> operands) {
 		super(operands.get(0).getType(), null, operands);
 		this.operator = operator.getValue();
-		this.operands = operands;	
+		this.type = operator.getType();
+		this.operands = operands;
 	}
 
 	public String getOperator() {
 		return operator;
+	}
+
+	public TokenType getOperatorType() {
+		return type;
 	}
 
 	public ArrayList<AstNode> getOperands() {
@@ -25,10 +31,10 @@ public class OperationNode extends AstNode {
 	}
 
 	@Override
-    public String toString() {
-        String operandsStr = operands.stream()
+	public String toString() {
+		String operandsStr = operands.stream()
 				.map(operand -> operand.toString())
 				.collect(Collectors.joining(", "));
-		return "OperationNode(" + operator + ":" + operandsStr +")";
-    }
+		return "OperationNode(" + operator + ":" + operandsStr + ")";
+	}
 }
