ab6ba5dc3a48e415a325b17efc42ec356b5b1de4
TovarishDru <dryshatu@yandex.ru>
2025-12-02 21:49:34 +0300

    fix: eval tests changed

--- test_files/test_8_eval.f
+++ test_files/test_8_eval.f
@@ -1,52 +1,33 @@
-(setq x 5)
-x
+(eval 5)
+(eval -1.35)
+(eval true)
+(eval null)
 
-((lambda (a b) (plus a b)) 55 -176)
-(setq myFunc (lambda (p) (cond (less p 0) plus minus)))
-((myFunc -1) 1 2)
-(((lambda (p) (cond (less p 0) plus minus)) 1) 1 2)
-
-(plus 5 3)
-
-(and true false)
-
-(setq  y (plus -1.35 2.15))
-(eval y)
-
-(func  square (n) (times n n))
-(square 5)
-
-(prog (a b) (setq a 5) (setq b 6) (plus a b))
-
-(cond (less 3 5) 1 0)
-
-(setq x 0)
-(while (less x 5) (setq x (plus x 1)))
-x
-
-(prog (n) (setq n 3) (return (times n n)) (plus n 10))
-
-(cons 1 (cons 2 (cons 3 null)))
-
-(head '(a b c))
-(tail '(234 321 222))
-(head (tail '(n y n)))
-(head (cons 'h (cons 'nothead (cons 'nothead null))))
-(head '(h nothead nothead))
-(and (less 1 2) (greater 5 3))
-
-(prog (x)
-  (setq x 'hello)
-  (cons x null))
-
-(prog (x)
-  (setq x 0)
-  (while (less x 10)
-    (cond (equal x 5) (break))
-    (setq x (plus x 1)))
-  x)
 
 (setq x 10)
-   (func test (x) (plus x 5))
-   (test 3)
-   x
+(eval x)
+
+(eval 'x)
+
+(eval '(plus 2 3))
+(eval '(minus 10 3))
+(eval '(times 2 (plus 3 4)))
+
+(setq expr '(plus 2 3))
+(eval expr)
+
+(setq t '(2 3))
+(setq expr (cons 'plus t))
+(eval expr)
+(setq expr (cons 'plus (tail '(1 2 3)
+
+
+
+(eval '(plus 2 (times 3 4)))
+
+(setq y (plus -1.35 2.15))
+(eval y)
+(setq expr '(plus -1.35 2.15))
+(eval expr)
+
+(eval '())
